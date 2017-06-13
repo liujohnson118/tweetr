41aa706a9eb394b458f28ebbdbc2793f74a25ba6
liujohnson118 <liujohnson118@yahoo.com>
2017-06-13 14:44:44 -0700

    hover state done

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,7 @@
+/*
+* Event handler for characeter counter in textarea for class new-tweet
+* Output: update remaning character count in the counter for a new tweet.
+*/
 $(document).ready(function(){
   const maxLength=Number($('.counter').html());
   $('.new-tweet').on('keyup','textarea',function(){
